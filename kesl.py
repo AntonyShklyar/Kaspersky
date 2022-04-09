601e6ade3182b7e3f452afb212ff68e05d1e414c
AntonyShklyar <95246873+AntonyShklyar@users.noreply.github.com>
2022-04-09 13:06:15 +0300

    Update kesl.py

--- kesl.py
+++ kesl.py
@@ -9,31 +9,14 @@ import socket
 import subprocess
 import re
 
-#Events are written to the log file /var/log/kasper.log. When the log size reaches 1 GB, it is deleted and recreated.
-if not os.path.exists('/var/log/kasper.log'): open("/var/log/kasper.log", "w+")
-#If the size of 1 GB is exceeded, the debug.log is deleted and recreated
-if not os.path.getsize('/var/log/kasper.log')/(1024*1024*1024)==0: os.remove('/var/log/kasper.log')
-
-def massive(IP=[]):
-	if set('01').issubset(socket.gethostname()):
-        	IPIZ = ["10.111.15.137", "10.111.16.195"]
-                IPVN = ['10.111.23.102', '10.149.25.102']
-                IPIN = ['10.111.33.67', '10.111.35.163']
-        elif set('02').issubset(socket.gethostname()):
-        	IPIZ = ['10.111.16.195', '10.111.15.137']
-                IPVN = ['10.149.25.102', '10.111.23.102']
-                IPIN = ['10.111.35.163', '10.111.33.67']	
-	if set('vn').issubset(socket.gethostname()):
-		for i in IPVN:
-			IP.append(i)
-	elif set('iz').issubset(socket.gethostname()):
-                for i in IPIZ:
-                        IP.append(i)
-	else:
-		for i in IPIN:
-                        IP.append(i)
-	return IP
+def logs():
+	'''Creating a session log and a log with a description of all sessions'''
+	#If the size of 1 GB is exceeded, the kasper.log is deleted and recreated
+	if not os.path.exists('/var/log/kasper.log'): f=open('/var/log/kasper.log', "w+"); f.close()
+	if not os.path.getsize('/var/log/kasper.log')/(1024*1024*1024)==0: os.system(r' >/var/log/kasper.log')
+	#The function returns 0
 def networkavailable(var, g):
+	'''Determining the network availability of KSC server'''
         mount = subprocess.Popen(("ping", "-c4", g), stdout=subprocess.PIPE); exit_code = subprocess.check_output(("sed", "-n", '1,/^---/d;s/%.*//;s/.*, //g;p;q'), stdin=mount.stdout); mount.wait()
         if int(exit_code.replace("\n","")) == 100:
                 if var==1:
@@ -45,10 +28,24 @@ def networkavailable(var, g):
         else:
                 os.system('echo $(date +"%Y%m%d-%H%M%S")   Server KSC is available      >> /var/log/backupdb.log')
                 return 0
-var=0
-my_array=massive()
-for g in my_array:
-        var += 1
+	'''
+	Return data type - number.
+	If the function returns 0 - KSC server is available
+	If the function returns 1 - KSC server isn't available
+	'''
+'''
+Editable parameters:
+--domain
+The data type is a dictionary.
+Data - domain: list of IP addresses of domain stores
+''''
+domain={'ac.com':['10.111.15.55', '10.111.15.64', '10.111.15.76'],'vp.com':['10.111.16.55', '10.111.16.64', '10.111.16.76'],'in.com':['10.111.17.54', '10.111.17.64', '10.111.17.76']}
+IP=[]
+logs()
+for x, y in domain.items():
+	if x in a:
+		IP.append(y)
+for var, g in enumerate(IP, 1):
         b=networkavailable(var, g)
         if b==0:
                 cmd = "/opt/kaspersky/klnagent64/bin/klnagchk | grep 'Server address' | awk '{print $3}' | head -n 1" 
@@ -59,10 +56,10 @@ for g in my_array:
 		if a==n:
                         exit()
 		else:
-			if subprocess.call(["/opt/kaspersky/klnagent64/bin/klmover", "-address", g]) == 0: os.system('echo $(date +"%Y%m%d-%H%M%S") KSC Server change operation Successful >> /var/log/kasper.log'); exit()
+			if subprocess.call(["/opt/kaspersky/klnagent64/bin/klmover", "-address", g]) == 0: os.system('echo $(date +"%Y%m%d-%H%M%S") KSC Server change operation on'+g+' Successful >> /var/log/kasper.log'); exit()
 			else:
-				os.system('echo $(date +"%Y%m%d-%H%M%S") KSC Server change operation Unsuccessful >> /var/log/kasper.log'); exit()
-	elif var != len(my_array):
+				os.system('echo $(date +"%Y%m%d-%H%M%S") KSC Server change operation on'+g+' Unsuccessful >> /var/log/kasper.log'); exit()
+	elif var != len(IP):
                 continue
         else:
                 exit()
